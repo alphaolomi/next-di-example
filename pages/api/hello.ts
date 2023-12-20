3e20b5675aa341a14976e7cce63dc0a0c5f74cbc
Alpha Olomi <alphaolomi@gmail.com>
2023-12-20 14:07:55 +0300

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import container from "@/container"
+import { TYPES } from '@/types'
+import { ApiService } from '@/interfaces'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const apiService = container.MyContainer.get<ApiService>(TYPES.ApiService)
+
+  const result = await apiService.fetchData()
+
+  res.status(200).json({ name: result })
 }
